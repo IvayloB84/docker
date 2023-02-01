8377d9db79092c31698d726c70782c20fe94ceb9
saywhat <72907892+IvayloB84@users.noreply.github.com>
2023-02-01 18:59:24 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+}
+provider "docker" {
+  host = "tcp://192.168.56.100:2375/"
+}
+
+resource "docker_image" "img-web" {
+  name = "shekeriev/bgapp-web"
+}
+
+resource "docker_network" "private_network" {
+  name = "app-net"
+}
+
+resource "docker_container" "con-web" {
+  name  = "con-web"
+  image = docker_image.img-web.latest
+  ports {
+    internal = "80"
+    external = "80"
+  }
+}
+
+resource "docker_image" "img-db" {
+  name = "shekeriev/bgapp-db"
+}

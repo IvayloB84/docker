0f890b2a8a62aaea976243d97fd658ad30188a29
saywhat <72907892+IvayloB84@users.noreply.github.com>
2023-02-01 19:37:32 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
 }
 provider "docker" {
 #  host = "tcp://192.168.56.102:2375/"
-   host = "ssh://jenkins@docker.lab:22"
+   host = "ssh://docker@docker.lab:22"
 }
 
 resource "docker_image" "img-web" {

bea35b8d7d2415a63e01ced1c6484272f72ad857
saywhat <72907892+IvayloB84@users.noreply.github.com>
2023-02-02 12:23:15 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,26 @@
-# Declaring the Required provider (Docker provider)
-terraform {
-  required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-    }
-  }
+# Creating a Docker Image ubuntu with the latest as the Tag
+resource "docker_image" "ubuntu" {               
+  name = "ubuntu:latest"
 }
 
-# Specifying the Docker provider configuration
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
+# Creating a Docker Container using the latest ubuntu image
+resource "docker_container" "my_container" {   
+  # Specifying the name of the container as my_container
+  name = "my_container"  
+  image = docker_image.ubuntu.latest       
+}
+
+# Creating a Docker Service named myservice using ubuntu image with a latest
+resource "docker_service" "my_service" {       
+  name = "myservice"
+  task_spec {
+   container_spec {
+     image = docker_image.ubuntu.latest     
+    }
+   }
+  endpoint_spec {
+    ports {
+     target_port = "8080"
+       }
+    }
 }

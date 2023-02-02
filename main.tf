b980f7d4c0a1e73d29ef9336529d5d1c63ae0074
saywhat <72907892+IvayloB84@users.noreply.github.com>
2023-02-02 13:39:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,28 @@
-# Creating a Docker Image ubuntu with the latest as the Tag
-resource "docker_image" "ubuntu" {               
-  name = "ubuntu:latest"
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+    }
+  }
 }
 
 resource "docker_network" "private_network" {
   name = "my_network"
   driver = "bridge"
 }
-# Creating a Docker Container using the latest ubuntu image
-resource "docker_container" "my_container" {   
-  # Specifying the name of the container as my_container
-  name = "my_container"  
-  image = docker_image.ubuntu.latest       
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
 }
 
-# Creating a Docker Service named myservice using ubuntu image with a latest
-resource "docker_service" "my_service" {       
-  name = "myservice"
-  task_spec {
-   container_spec {
-     image = docker_image.ubuntu.latest     
-    }
-   }
-  endpoint_spec {
-    ports {
-     target_port = "8080"
-       }
-    }
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
 }
